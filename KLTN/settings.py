41a781179cd84221ae4f7959e5fe2ce9304b16f5
Võ Thanh Trường Quang <vothanhtruongquang@gmail.com>
2019-03-28 18:38:31 +0700

    add elastisearch

--- KLTN/settings.py
+++ KLTN/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_elasticsearch_dsl',
     'corsheaders',
     'prettyjson',
     'rest_framework',
