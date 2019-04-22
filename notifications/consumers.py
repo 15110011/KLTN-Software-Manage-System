8398dea02d5982a6c45f1b2fd52957e98cd6e4c7
Võ Thanh Trường Quang <vothanhtruongquang@gmail.com>
2019-04-22 16:51:36 +0700

    init websocket generics

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,16 +1,27 @@
 from django.contrib.auth import get_user_model
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.consumer import AsyncConsumer
+from channels.generic.websocket import WebsocketConsumer
+from .models import Notification
+import json
+from . import serializers
 
-
-class NotificationConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class NotificationConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
         self.user_id = self.scope['url_route']['kwargs']['user_id']
-        await self.accept()
-        await self.send('hello')
-       
+        user_notifications = self.get_notifications(self.user_id)
+        self.send(json.dumps({'notifications': user_notifications}))
 
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         pass
-    
-    async def receive(self, text_data):
-        print (text_data)
+
+    def receive(self, text_data):
+        print(text_data)
+
+    def get_notifications(self, user_id):
+        queryset = Notification.objects.filter(user=user_id)
+        data = serializers.NotificationSerializer(queryset, many=True).data
+        return data
+
+    def create_notifications(self, data):
+        pass    
