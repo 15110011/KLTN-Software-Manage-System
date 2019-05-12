0d9df48d01d39da8f01af38d3d3ce70eb6eaea97
Võ Thanh Trường Quang <vothanhtruongquang@gmail.com>
2019-05-12 13:01:49 +0700

    get mails from gmail

--- mail_box/inbox/gmail_utils.py
+++ mail_box/inbox/gmail_utils.py
@@ -2,6 +2,7 @@ from googleapiclient.discovery import build
 from oauth2client import file, client, tools
 from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
+from httplib2 import Http
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,7 +11,7 @@ SCOPES = [
     'https://mail.google.com/',
     'https://www.googleapis.com/auth/gmail.modify',
     'https://www.googleapis.com/auth/gmail.readonly',
-    'https://www.googleapis.com/auth/gmail.metadata'
+    # 'https://www.googleapis.com/auth/gmail.metadata'
 ]
 
 # credentials = service_account.Credentials.from_service_account_file(
@@ -18,12 +19,17 @@ SCOPES = [
 # service = build('gmail', 'v1', credentials=credentials)
 
 def get_service():
-  flow = InstalledAppFlow.from_client_secrets_file(
-                CREDENTIALS_JSON_FILE, SCOPES)
-  creds = flow.run_local_server()
-  service = build('gmail', 'v1', credentials=creds)
-  results = service.users().messages().list(userId='me', q='').execute()
-  print (results)
+  store = file.Storage('token.json')
+  creds = store.get()
+  if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets(CREDENTIALS_JSON_FILE, SCOPES)
+    creds = tools.run_flow(flow, store)
+  service = build('gmail', 'v1', http=creds.authorize(Http()))
+  results = service.users().messages().list(userId='me', labelIds = ['INBOX']).execute()
+  messages = results.get('messages', [])
+  for message in messages:
+    msg = service.users().messages().get(userId='me', id=message['id']).execute()
+    print (msg['snippet'])
   return results
 
 get_service()
