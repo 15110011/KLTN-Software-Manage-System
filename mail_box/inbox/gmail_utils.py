e56c0b948a568766086b13413fdb4d72be77d555
Võ Thanh Trường Quang <vothanhtruongquang@gmail.com>
2019-05-20 09:54:48 +0700

    send email & message to client

--- mail_box/inbox/gmail_utils.py
+++ mail_box/inbox/gmail_utils.py
@@ -1,7 +1,11 @@
 from googleapiclient.discovery import build
 from oauth2client import file, client, tools
 from google.oauth2 import service_account
-from google_auth_oauthlib.flow import InstalledAppFlow
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 # from google.cloud import pubsub_v1
 from httplib2 import Http
 import os
@@ -28,7 +32,7 @@ class GmailService:
 
     def get_service(self):
         store = file.Storage(os.path.join(
-        BASE_DIR, 'inbox/token.json'))
+            BASE_DIR, 'inbox/token.json'))
         creds = store.get()
         if not creds or creds.invalid:
             flow = client.flow_from_clientsecrets(
@@ -49,15 +53,19 @@ class GmailService:
         service = self.get_service()
         msg = service.users().messages().get(userId='me', id=message_id).execute()
         message = None
+        subject = [s['value']
+                   for s in msg['payload']['headers'] if s['name'] == 'Subject']
+        from_email = [f['value']
+                      for f in msg['payload']['headers'] if f['name'] == 'From']
         if 'data' in msg['payload']['body']:
             message = msg['payload']['body']['data']
             message = base64.urlsafe_b64decode(message)
             message = str(message, 'utf-8')
-        elif 'data' in msg['payload']['parts'][0]['body']:
-            message = msg['payload']['parts'][0]['body']['data']
+        elif 'data' in msg['payload']['parts'][1]['body']:
+            message = msg['payload']['parts'][1]['body']['data']
             message = base64.urlsafe_b64decode(message)
             message = str(message, 'utf-8')
-        return {"message": message, "historyId": msg['historyId']}
+        return {"message": message, "history_id": msg['historyId'], "subject": subject, "from": from_email}
 
     def watch_mail_box(self):
         service = self.get_service()
@@ -79,9 +87,30 @@ class GmailService:
             userId='me', startHistoryId=history_id, historyTypes=historyTypes,
             maxResults=maxResults, pageToken=pageToken, labelId=labelId).execute()
         data = []
+        if history.get('history', None) is None:
+            return {"messages": []}
         for messages in history['history']:
             for msg in messages['messages']:
                 data.append(
                     {"message_id": msg['id'], "thread_id": msg['threadId']})
 
         return {"messages": data}
+
+    def send_message(self, data, threadId=None):
+        service = self.get_service()
+        message = MIMEText(data['message'])
+        message['to'] = data['to']
+        message['from'] = data['from']
+        message['subject'] = data['subject']
+        raw = base64.urlsafe_b64encode(
+            message.as_bytes())
+        email = service.users().messages().send(
+            userId='me', body=raw, threadId=threadId)
+        return {"message_id": email['id'], "thread_id": email['threadId']}
+
+data['to'] = 'vothanhtruongquang@gmail.com'
+data['from'] = 'theaqvteam@gmail.com'
+data['subject'] = 'Test gui email'
+data['message'] = 'Thu coi gui duoc ko'
+gmail = GmailService()
+gmail.send_message(data)
--- mail_box/inbox/models.py
+++ mail_box/inbox/models.py
@@ -3,11 +3,17 @@ from django.contrib.auth.models import User
 from config.base_models import BaseModel
 # Create your models here.
 
+EMAIL_TYPE = (
+    ('SENT', 'SENT'),
+    ('INBOX', 'INBOX'),
+    
+)
 
 class MailBox(BaseModel):
     user_id = models.CharField(max_length=255)
     message_id = models.CharField(max_length=255)
     thread_id = models.CharField(max_length=255)
+    email_type = models.CharField(max_length=20, choices=EMAIL_TYPE)
 
 class MailHistory(BaseModel):
     history_id = models.CharField(max_length=255)
