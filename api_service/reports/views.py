6f70c2784851408adcf54807b4d0e3afa8035048
Võ Thanh Trường Quang <vothanhtruongquang@gmail.com>
2019-05-24 17:49:03 +0700

    filter report

--- api_service/reports/views.py
+++ api_service/reports/views.py
@@ -35,18 +35,20 @@ def Reports(request):
         result = filter_state(states)
     if states is None:
         result = filter_time(from_date, to_date)
-        print (result)
-        
+    if states and from_date and to_date:
+        result = filter_state_datetime(states, from_date, to_date)
 
     return Response({"data": result})
 
-
-def filter_state(states):
-    orders = Order.objects.filter(Q(status='RUNNING') | Q(status='COMPLETED'))
+def filter_state_datetime(states, from_date, to_date):
+    filters = Q()
+    filters.add(Q(created__gte=from_date) & Q(created__lte=to_date), Q.AND)
+    filters.add(Q(status='RUNNING') | Q(status='COMPLETED'), Q.AND)
+    orders = Order.objects.filter(filters)
     data = []
     for order in orders:
         conditions = order.campaign.marketing_plan.condition['must']
-        is_all=True
+        is_all = True
         for condition in conditions:
             if condition['operand'] == '1':
                 if condition['operator'] == 'Equal to':
@@ -57,15 +59,39 @@ def filter_state(states):
                     if not set(condition['data']).intersection(set(states)):
                         result = ReportOrderSerializer(order).data
                         data.append(result)
-                is_all=False
+                is_all = False
         if is_all:
             result = ReportOrderSerializer(order).data
             data.append(result)
     return data
 
+def filter_state(states):
+    orders = Order.objects.filter(Q(status='RUNNING') | Q(status='COMPLETED'))
+    data = []
+    for order in orders:
+        conditions = order.campaign.marketing_plan.condition['must']
+        is_all = True
+        for condition in conditions:
+            if condition['operand'] == '1':
+                if condition['operator'] == 'Equal to':
+                    if set(condition['data']).intersection(set(states)):
+                        result = ReportOrderSerializer(order).data
+                        data.append(result)
+                elif condition['operator'] == 'Not equal to':
+                    if not set(condition['data']).intersection(set(states)):
+                        result = ReportOrderSerializer(order).data
+                        data.append(result)
+                is_all = False
+        if is_all:
+            result = ReportOrderSerializer(order).data
+            data.append(result)
+    return data
+
+
 def filter_time(from_date, to_date):
     filters = Q()
     filters.add(Q(created__gte=from_date) & Q(created__lte=to_date), Q.AND)
+    filters.add(Q(status='RUNNING') | Q(status='COMPLETED'), Q.AND)
     orders = Order.objects.filter(filters)
     orders = [ReportOrderSerializer(order).data for order in orders]
-    return orders  
+    return orders
